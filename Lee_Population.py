614672b36987ec59ea7bcd26affe12a9ed12dfe8
ShadowCab <bass-guy@cfl.rr.com>
2022-05-23 16:52:44 -0400

    Create Lee_Population.py

---
+++ Lee_Population.py
@@ -0,0 +1,5 @@
+//Author Name: James Lee
+//Date: 6/5/2022
+//Program Name: Lee_Population.py
+//Purpose: Given starting number, average daily increase, and number of days of growth,
+//         display a table showing ~size of population for each day.
